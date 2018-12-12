a59d203c687e22bc3139b7b825d04c7c7d157403
C-D-Lewis <Bonsitm@gmail.com>
2018-12-12 13:14:28 +0000

    Simplify test-app

--- test-app/index.js
+++ test-app/index.js
@@ -10,14 +10,13 @@ const state = {
 };
 
 const actions = {
-  // Setters
   setUrl: url => state => ({ url }),
   setJsonString: jsonString => state => ({ jsonString }),
   setIsValid: isValid => state => ({ isValid }),
   setTrace: trace => state => ({ trace }),
+  validateUrl: el => (state, actions) => {
+    actions.setUrl(el.target.value);
 
-  // Tasks
-  doValidation: () => (state, actions) => {
     try {
       const dl = DigitalLink(state.url);
       actions.setIsValid(dl.isValid());
@@ -35,37 +34,28 @@ const actions = {
 const Paragraph = (props, children) => <div class="paragraph">{children}</div>;
 
 const TraceView = ({ trace }) => (trace.length > 0 && 
-  <div class="trace-wrapper">
-    <table>
-      <tr><th>Step</th><th>Rule</th><th>Matched</th><th>Remainder</th></tr>
-      {trace.map((item, i) => (
-        <tr class="trace-item">
-          <td>{i + 1}</td><td>{item.rule}</td><td>{item.match}</td><td>{item.remainder}</td>
-        </tr>
-      ))}
-    </table>
-  </div>
-);
+  <table class="trace-wrapper">
+    <tr><th>Step</th><th>Rule</th><th>Matched</th><th>Remainder</th></tr>
+    {trace.map((item, i) => (
+      <tr class="trace-item">
+        <td>{i + 1}</td><td>{item.rule}</td><td>{item.match}</td><td>{item.remainder}</td>
+      </tr>
+    ))}
+  </table>);
 
 const view = (state, actions) => (
   <div class="col-centered">
     <h1 class="title">Example Digital Link Verifier</h1>
     <Paragraph>Enter your GS1 Digital Link below to see if it is valid.</Paragraph>
-    <input type="text" class="input-url" value={state.url} 
-      style={{ borderBottom: `solid 4px ${state.isValid === true ? 'limegreen' : 'red'}` }}
-      oninput={(el) => {
-        actions.setUrl(el.target.value);
-        actions.doValidation();
-      }}/>
+    <input type="text" class="input-url" value={state.url} oninput={actions.validateUrl}
+      style={{ borderBottom: `solid 4px ${state.isValid === true ? 'limegreen' : 'red'}` }}/>
     <Paragraph>
       Powered by the <a href="https://github.com/evrythng/digital-link.js" target="_blank">
         digital-link.js</a> SDK.
     </Paragraph>
-
     <TraceView trace={state.trace}/>
-
     <textarea class="state-view" disabled>{state.jsonString}</textarea>
   </div>
 );
 
-app(state, actions, view, document.getElementById('app'));
+app(state, actions, view, document.body);
